896d976cb4d9cfcbaac43aebde4933f30bf2e79f
critr <cosaswm@gmail.com>
2026-03-12 11:52:38 +0100

    Define interfaces for services

---
+++ BestStories.Api/Services/Interfaces/IBestStoriesService.cs
@@ -0,0 +1,8 @@
+﻿using BestStories.Api.Models;
+
+namespace BestStories.Api.Services.Interfaces;
+
+public interface IBestStoriesService
+{
+    Task<IReadOnlyList<BestStoryDto>> GetBestStoriesAsync(int n, CancellationToken ct);
+}
---
+++ BestStories.Api/Services/Interfaces/IHackerNewsClient.cs
@@ -0,0 +1,9 @@
+﻿using BestStories.Api.Models;
+
+namespace BestStories.Api.Services.Interfaces;
+
+public interface IHackerNewsClient
+{
+    Task<IReadOnlyList<int>> GetBestStoryIdsAsync(CancellationToken ct);
+    Task<HnItem?> GetItemAsync(int id, CancellationToken ct);
+}
