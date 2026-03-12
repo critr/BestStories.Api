2e0605cc328c569063315c87e4915a401efb71f4
critr <cosaswm@gmail.com>
2026-03-12 17:13:12 +0100

    Add wiring

--- BestStories.Api/Program.cs
+++ BestStories.Api/Program.cs
@@ -1,20 +1,29 @@
+using BestStories.Api.Services.Implementations;
+using BestStories.Api.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddHttpClient<IHackerNewsClient, HackerNewsClient>(client =>
+{
+    client.BaseAddress = new Uri("https://hacker-news.firebaseio.com/v0/");
+    client.Timeout = TimeSpan.FromSeconds(5);
+});
+
+builder.Services.AddScoped<IBestStoriesService, BestStoriesService>();
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
