f1172b1d8015ba46775c31eabd67e14073b5b4b2
critr <cosaswm@gmail.com>
2026-03-12 08:38:58 +0100

    Add models

---
+++ BestStories.Api/Models/BestStoriesDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BestStories.Api.Models;
+
+public sealed record BestStoryDto
+{
+    public string Title { get; init; } = string.Empty;
+    public string Uri { get; init; } = string.Empty;
+    public string PostedBy { get; init; } = string.Empty;
+    public DateTimeOffset Time { get; init; }
+    public int Score { get; init; }
+    public int CommentCount { get; init; }
+}
---
+++ BestStories.Api/Models/HnItem.cs
@@ -0,0 +1,13 @@
+﻿namespace BestStories.Api.Models;
+
+public sealed record HnItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+    public string By { get; set; } = string.Empty;
+    public long Time { get; set; }
+    public int Score { get; set; }
+    public int Descendants { get; set; }
+    public string Type { get; set; } = string.Empty;
+}
